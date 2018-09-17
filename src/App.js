bf584b6a4eeda2d55b4fb67b383f4f8125c9dacb
MOKESH tringapps <mokesh.s@tringapps.com>
2018-09-17 12:56:39 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import Footer from './Components/Footer/Footer.jsx';
 // import SideNav from './Components/SideNav/SideNav.jsx';
 import Content from './Components/Content/Content.jsx';
 
-class App extends Component {
-
-  // <Content myNumber = {this.state.data}></Content>
-  
+class App extends Component {  
   render() {
     return (
       <div id="outer">
