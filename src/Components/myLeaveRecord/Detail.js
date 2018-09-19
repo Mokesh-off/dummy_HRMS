6f71c7fde11dce06411224bf244795388a49d7fc
Swathi Sampath <swathi.s@tringapps.com>
2018-09-19 11:41:42 +0530

    changes in Json object

---
+++ src/Components/myLeaveRecord/Detail.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react"
+import MyLeaveRecord from "./MyLeaveRecord.js"
+
+
+
+class Details extends Component {
+ constructor(props){
+	 super(props);
+	 
+	// this.getData = this.getData.bind(this);
+ }
+
+	
+	render() {
+    return(
+			<div>
+				<label>Test</label>
+			</div>
+    );
+  }
+}
+
+export default Details; 
