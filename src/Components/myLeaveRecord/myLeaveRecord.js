afef12eacc9588f796a0b394af0984a76500a467
Rupali <rupali@Tringapps-Admins-Mac-mini-3.local>
2018-09-19 14:33:03 +0530

    LeaveReq

--- src/Components/myLeaveRecord/myLeaveRecord.js
+++ src/Components/myLeaveRecord/myLeaveRecord.js
@@ -1,66 +1,8 @@
-import React, {Component} from "react"
-import './MyLeaveRecord.css'
-
-import lists from './MyLeaveRecord.js'
-
-import Details from './Detail.js'
-
-class MyLeaveRecord extends Component {
- constructor(props){
-	 super(props);
-	 this.state={
-		 "employee":lists,
-		 "detailStatus": false,
-		 resultValue:""
-	 }
-	// this.getData = this.getData.bind(this);
- }
-	getData (target,employee) {
-		//console.log(target);
-		this.setState({resultValue  : this.state.employee.find(value => value.id == target.id)});
-		
-		//console.log(this.props.resultValue);
-		this.setState({detailStatus : this.state.detailStatus = true});
-	}
-	getComponent() {
-		if (this.state.detailStatus) {  // show the modal if state showModal is true
-		  return <Details />;
-		} else {
-		  return null;
-		}
-	  }
-	
-	render() {
-    return(
-			<div>
-				<table>
-					<thead>
-					<tr>
-						<th>Type of leave</th>
-						<th>Apply Date</th>
-						<th>Apply Time</th>
-						<th>Status</th>
-					</tr>
-					</thead>
-					<tbody>
-						{
-							this.state.employee.map((employee) =>
-							<tr key={employee.id} onClick={this.getData.bind(this,employee)}>
-							<td>{employee.type}</td>
-							<td>{employee.applyDate}</td>
-							<td>{employee.applyTime}</td>
-							<td>{employee.state}</td>
-							</tr>
-						)
-						}
-					</tbody>
-				</table>
-				
-					<Details show={this.state.detailStatus == true}/>
-				
-			</div>
-    );
-  }
-}
-
-export default MyLeaveRecord 
+const lists = [
+	{id:"1",type:"LOP",applyDate:"12-01-2018",applyTime:"15:01",state:"decline"},
+	{id:"2",type:"CL",applyDate:"12-02-2018",applyTime:"15:02",state:"decline"},
+	{id:"3",type:"EL",applyDate:"24-05-2018",applyTime:"20:01",state:"decline"},
+	{id:"4",type:"LOP",applyDate:"12-07-2018",applyTime:"15:01",state:"decline"},
+	{id:"5",type:"EL",applyDate:"10-08-2018",applyTime:"25:01",state:"decline"}
+];
+export default  lists;
